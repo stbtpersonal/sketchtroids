39df24c7680a542a0d415508b9a4e21ef9dfa5a3
stbt <stbt.personal@gmail.com>
2018-03-04 22:10:45 +0200

    Set up a requestAnimationFrame loop.

--- Source/Main.hs
+++ Source/Main.hs
@@ -2,9 +2,24 @@ module Main where
 
 import Haste.DOM as DOM
 import Other
+import Haste.Graphics.AnimationFrame as AnimationFrame
 
 main :: IO ()
 main = do
-    let number = Other.calculate 12
+    divElement <- DOM.newElem "div"
+    DOM.setAttr divElement "id" "text"
+    DOM.appendChild DOM.documentBody divElement
+
+    AnimationFrame.requestAnimationFrame mainLoop
+    return ()
+
+
+mainLoop :: AnimationFrame.HRTimeStamp -> IO ()
+mainLoop timestamp = do
+    Just divElement <- DOM.elemById "text"
+    DOM.clearChildren divElement
+    let number = Other.calculate $ round timestamp
     textElement <- DOM.newTextElem $ "Hello World!!! " ++ show number
-    DOM.appendChild DOM.documentBody textElement
+    DOM.appendChild divElement textElement
+    AnimationFrame.requestAnimationFrame mainLoop
+    return ()
