307f81023a1b2f883d20beb621f769290fe1c633
stbt <stbt.personal@gmail.com>
2018-03-06 00:06:25 +0200

    Brought in a nicer HTML template. Created a canvas and made the FPS render on it.

--- Source/GameState.hs
+++ Source/GameState.hs
@@ -1,11 +1,18 @@
 module GameState where
 
+import Haste.Graphics.Canvas as Canvas
 import Fps
 
-data GameState = GameState { _fps :: Fps }
+data GameState = GameState { _fps :: Fps, _canvas :: Canvas, _rotation :: Double }
 
 getFps :: GameState -> Fps
 getFps (GameState { _fps = fps }) = fps
 
-defaultValue :: GameState
-defaultValue = GameState { _fps = Fps.defaultValue }
+getCanvas :: GameState -> Canvas
+getCanvas (GameState { _canvas = canvas }) = canvas
+
+getRotation :: GameState -> Double
+getRotation (GameState { _rotation = rotation }) = rotation
+
+new :: Canvas -> GameState
+new canvas = GameState { _fps = Fps.defaultValue, _canvas = canvas, _rotation = 0 }
--- Source/Main.hs
+++ Source/Main.hs
@@ -3,8 +3,15 @@ module Main where
 import Control.Monad
 import Haste.DOM as DOM
 import Haste.Graphics.AnimationFrame as AnimationFrame
+import Haste.Graphics.Canvas as Canvas
 import GameState
 import Fps
+
+nativeWidth :: Double
+nativeWidth = 800
+
+nativeHeight :: Double
+nativeHeight = 600
 
 main :: IO ()
 main = do
@@ -12,27 +19,40 @@ main = do
     DOM.setAttr divElement "id" "text"
     DOM.appendChild DOM.documentBody divElement
 
-    let initialState = GameState.defaultValue
-    AnimationFrame.requestAnimationFrame $ mainLoop initialState
+    canvas <- Main.createCanvas
+    Just contentElement <- DOM.elemById "content"
+    DOM.appendChild contentElement canvas
+
+    let initialState = GameState.new canvas
+    AnimationFrame.requestAnimationFrame $ Main.mainLoop initialState
     return ()
+
+createCanvas :: IO Canvas
+createCanvas = do
+    canvasElement <- DOM.newElem "canvas"
+    DOM.setAttr canvasElement "id" "canvas"
+    DOM.setAttr canvasElement "width" $ show Main.nativeWidth
+    DOM.setAttr canvasElement "height" $ show Main.nativeHeight
+    Just canvas <- DOM.fromElem canvasElement
+    return canvas
 
 mainLoop :: GameState -> AnimationFrame.HRTimeStamp -> IO ()
 mainLoop state timestamp = do
     let fps = GameState.getFps state
-    let (updatedFps, fpsToDisplay) = updateFps fps timestamp
+    let (updatedFps, deltaTime, fpsToDisplay) = Main.updateFps fps timestamp
+
+    let rotation = GameState.getRotation state
+    let updatedRotation = rotation + (0.0001 * deltaTime)
 
     when (fpsToDisplay >= 0) $ do
-        Just divElement <- DOM.elemById "text"
-        DOM.clearChildren divElement
-        textElement <- DOM.newTextElem $ "FPS: " ++ show fpsToDisplay
-        DOM.appendChild divElement textElement
+        Main.render (GameState.getCanvas state) fpsToDisplay updatedRotation
 
-    let newState = GameState { _fps = updatedFps }
+    let newState = state { _fps = updatedFps, _rotation = updatedRotation }
 
-    AnimationFrame.requestAnimationFrame $ mainLoop newState
+    AnimationFrame.requestAnimationFrame $ Main.mainLoop newState
     return ()
 
-updateFps :: Fps -> Double -> (Fps, Integer)
+updateFps :: Fps -> Double -> (Fps, Double, Integer)
 updateFps fps timestamp =
     let
         previousTimestamp = Fps.getTimestamp fps
@@ -44,4 +64,40 @@ updateFps fps timestamp =
             | otherwise                 = (candidateTimeCount - 1000, 1, candidateFrameCount)
         updatedFps = Fps { _timeCount = updatedTimeCount, _frameCount = updatedFrameCount, _timestamp = timestamp }
     in
-        (updatedFps, fpsToDisplay)
+        (updatedFps, deltaTime, fpsToDisplay)
+
+render :: Canvas -> Integer -> Double -> IO ()
+render canvas fps rotation = do
+    Canvas.render canvas $ do 
+        Main.background
+        Main.square rotation
+        Main.text fps
+    return ()
+
+background :: Picture ()
+background = 
+    let 
+        shape = Canvas.rect (0, 0) (Main.nativeWidth, Main.nativeHeight)
+        filledShape = Canvas.fill shape
+        coloredShape = Canvas.color (Canvas.RGB 255 255 255) filledShape
+    in
+        coloredShape
+
+square :: Double -> Picture ()
+square rotation =
+    let
+        shape = Canvas.rect (-25, -25) (25, 25)
+        stroked = Canvas.stroke shape
+        colored = Canvas.color (Canvas.RGB 255 0 0) stroked
+        rotated = Canvas.rotate rotation colored
+        translated = Canvas.translate (Main.nativeWidth / 2, Main.nativeHeight / 2) rotated
+    in
+        translated
+
+text :: Integer -> Picture ()
+text fps =
+    let
+        text = Canvas.text (5, 22.5) $ "FPS: " ++ show fps
+        fonted = Canvas.font "20px sans-serif" text
+    in
+        fonted
