1c0dfe70622fc24391ea766ebd58e27d458c9b45
stbt <stbt.personal@gmail.com>
2018-03-19 21:47:53 +0200

    Added a little beat to the end of the loading spinner, in case I want to do something like that.

--- Source/Modes/LoadingMode/Entities/Loading.hs
+++ Source/Modes/LoadingMode/Entities/Loading.hs
@@ -12,19 +12,22 @@ module Modes.LoadingMode.Entities.Loading(Loading(Loading), new) where
     import Haste
     import Data.Fixed
 
-    data Loading = Loading { elapsedTime :: Double, rotation :: Double }
+    data Loading = Loading { elapsedTime :: Double, rotation :: Double, scale :: Double }
 
     new :: Loading
-    new = Loading { elapsedTime = 0, rotation = 0 }
+    new = Loading { elapsedTime = 0, rotation = 0, Modes.LoadingMode.Entities.Loading.scale = 1 }
 
     imageDef :: Resources.ResourceDef
     imageDef = (ResourceKey "Loading", "Resources/Loading.png")
 
+    rotationStartTime :: Double
+    rotationStartTime = 800
+
+    scaleStartTime :: Double
+    scaleStartTime = 1900
+
     totalTime :: Double
     totalTime = 2000
-
-    restingTime :: Double
-    restingTime = 800
 
     instance EntityClass Loading where
 
@@ -33,16 +36,19 @@ module Modes.LoadingMode.Entities.Loading(Loading(Loading), new) where
         update loading@Loading{elapsedTime} input@Input{deltaTime} = 
             let
                 moduloElapsedTime = (elapsedTime  + deltaTime) `mod'` totalTime
-                phase = if moduloElapsedTime <= restingTime then 0 else (moduloElapsedTime - restingTime) / (totalTime - restingTime)
-                updatedRotation = -(28.43811 * phase) + (188.4375 * phase ^ 2) - (274.2243 * phase ^ 3) + (120.5082 * phase ^ 4)
+                rotationPhase = if moduloElapsedTime <= rotationStartTime then 0 else (moduloElapsedTime - rotationStartTime) / (totalTime - rotationStartTime)
+                rotation = -(28.43811 * rotationPhase) + (188.4375 * rotationPhase ^ 2) - (274.2243 * rotationPhase ^ 3) + (120.5082 * rotationPhase ^ 4)
+                scalePhase = if moduloElapsedTime <= scaleStartTime then 1 else (moduloElapsedTime - scaleStartTime) / (totalTime - scaleStartTime)
+                scale = 1 + (0.1 * scalePhase) - (0.1 * scalePhase ^ 2)
             in
-                Entity $ loading { elapsedTime = moduloElapsedTime, rotation = updatedRotation }
+                Entity $ loading { elapsedTime = moduloElapsedTime, rotation = rotation, Modes.LoadingMode.Entities.Loading.scale = scale }
 
-        render loading@Loading{rotation} resources@Resources{images} = 
+        render loading@Loading{rotation, Modes.LoadingMode.Entities.Loading.scale} resources@Resources{images} = 
             let
                 (bitmap, (width, height)) = images ! (fst imageDef)
                 drawnSprite = Canvas.draw bitmap (-(width / 2), -(height / 2))
                 rotatedSprite = Canvas.rotate rotation drawnSprite
-                translatedSprite = Canvas.translate (Constants.nativeWidth / 2, Constants.nativeHeight / 2) rotatedSprite
+                scaledSprite = Canvas.scale (scale, scale) rotatedSprite
+                translatedSprite = Canvas.translate (Constants.nativeWidth / 2, Constants.nativeHeight / 2) scaledSprite
             in
                 translatedSprite
