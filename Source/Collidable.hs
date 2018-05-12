5c25c0df14e9390b25837590f45c7883af055574
stbt <stbt.personal@gmail.com>
2018-05-12 13:57:19 +0300

    Implemented a circle collision test which is probably more appropriate here.

--- Source/Collidable.hs
+++ Source/Collidable.hs
@@ -9,8 +9,8 @@ module Collidable where
 
     class Sprite a => Collidable a where
 
-        haveCollided :: Collidable b => a -> b -> Resources -> Bool
-        haveCollided a b resources = 
+        haveCollidedAabb :: Collidable b => a -> b -> Resources -> Bool
+        haveCollidedAabb a b resources = 
             let
                 boundingBoxA = boundingBox a resources
                 boundingBoxB = boundingBox b resources
@@ -19,3 +19,21 @@ module Collidable where
                 (Rectangle.left boundingBoxA <= Rectangle.right boundingBoxB) &&
                 (Rectangle.bottom boundingBoxA >= Rectangle.top boundingBoxB) &&
                 (Rectangle.top boundingBoxA <= Rectangle.bottom boundingBoxB)
+
+        haveCollidedCircle :: Collidable b => a -> b -> Resources -> Bool
+        haveCollidedCircle a b resources =
+            let
+                radius c = sqrt ((Sprite.width c resources ** 2) + (Sprite.height c resources ** 2)) / 2
+                radiusA = radius a
+                radiusB = radius b
+
+                positionA = Sprite.position' a
+                positionB = Sprite.position' b
+                dx = (Point.x positionB) - (Point.x positionA)
+                dy = (Point.y positionB) - (Point.y positionA)
+                distance = sqrt ((dx ** 2) + (dy ** 2))
+            in
+                distance < radiusA + radiusB
+
+        haveCollided :: Collidable b => a -> b -> Resources -> Bool
+        haveCollided = haveCollidedCircle
