f53e3da29ddbaf28d16c4c4245836c1ffdb7f242
stbt <stbt.personal@gmail.com>
2018-06-16 18:07:53 +0300

    Made smaller asteroids fly and rotate faster.

--- Source/Asteroid.hs
+++ Source/Asteroid.hs
@@ -12,7 +12,7 @@ module Asteroid
     import Point (Point(Point, x, y))
     import Entity (EntityClass(load, update, render), Entity(Entity))
     import Resources (Resources(Resources, images), ResourceKey(ResourceKey), ResourceDef)
-    import Input (Input(Input, deltaTime, randomGenerator), randomize)
+    import Input (Input(Input, deltaTime, randomGenerator), randomize, randomizeNext)
     import System.Random as Random (randomR)
     import Utils (wrap)
     import Constants (nativeWidth, nativeHeight)
@@ -40,11 +40,11 @@ module Asteroid
         , _rotation = 0
         , _rotationVelocity = 0
         , _isInitialized = False
-        , _arrivingDirection = fromLeft
+        , _arrivingDirection = fromLeft bigAsteroidInfo
         , _hasArrived = False
         , _isEnabled = True
         , _timesHit = 0
-        , _asteroidType = asteroid1
+        , _asteroidType = bigAsteroid1
         }
 
     imageDefs' :: [Resources.ResourceDef]
@@ -57,50 +57,69 @@ module Asteroid
 
     data AsteroidType = AsteroidType
         { _spriteIndex :: Int
+        , _asteroidInfo :: AsteroidInfo
         , _breaksInto :: [AsteroidType]
         }
 
-    asteroid1 :: AsteroidType
-    asteroid1 = AsteroidType
-        {_spriteIndex = 0
-        , _breaksInto = [asteroidSmall1, asteroidSmall1, asteroidSmall1]
+    bigAsteroid1 :: AsteroidType
+    bigAsteroid1 = AsteroidType
+        { _spriteIndex = 0
+        , _asteroidInfo = bigAsteroidInfo
+        , _breaksInto = [smallAsteroid1, smallAsteroid1, smallAsteroid1]
         }
 
-    asteroid2 :: AsteroidType
-    asteroid2 = AsteroidType
+    bigAsteroid2 :: AsteroidType
+    bigAsteroid2 = AsteroidType
         { _spriteIndex = 1
-        , _breaksInto = [asteroidSmall1, asteroidSmall1]
+        , _asteroidInfo = bigAsteroidInfo
+        , _breaksInto = [smallAsteroid1, smallAsteroid1]
         }
 
-    asteroid3 :: AsteroidType
-    asteroid3 = AsteroidType
+    bigAsteroid3 :: AsteroidType
+    bigAsteroid3 = AsteroidType
         { _spriteIndex = 2
-        , _breaksInto = [asteroidSmall1]
+        , _asteroidInfo = bigAsteroidInfo
+        , _breaksInto = [smallAsteroid1]
         }
 
-    asteroidSmall1 :: AsteroidType
-    asteroidSmall1 = AsteroidType
+    smallAsteroid1 :: AsteroidType
+    smallAsteroid1 = AsteroidType
         { _spriteIndex = 3
+        , _asteroidInfo = smallAsteroidInfo
         , _breaksInto = []
         }
 
     bigAsteroidTypes :: [AsteroidType]
-    bigAsteroidTypes = [asteroid1, asteroid2, asteroid3]
+    bigAsteroidTypes = [bigAsteroid1, bigAsteroid2, bigAsteroid3]
 
-    minVelocity :: Double
-    minVelocity = 0.05
+    data AsteroidInfo = AsteroidInfo
+        { _minVelocity :: Double
+        , _maxVelocity :: Double
+        , _arrivalVelocityMultiplier :: Double
+        , _maxRotationVelocity :: Double
+        , _maxTimesHit :: Integer
+        }
 
-    maxVelocity :: Double
-    maxVelocity = 0.1
+    bigAsteroidInfo :: AsteroidInfo
+    bigAsteroidInfo = AsteroidInfo
+        { _minVelocity = 0.05
+        , _maxVelocity = 0.1
+        , _arrivalVelocityMultiplier = 0.5
+        , _maxRotationVelocity = 0.001
+        , _maxTimesHit = 5
+        }
 
-    maxRotationVelocity :: Double
-    maxRotationVelocity = 0.001
+    smallAsteroidInfo :: AsteroidInfo
+    smallAsteroidInfo = AsteroidInfo
+        { _minVelocity = 0.1
+        , _maxVelocity = 0.2
+        , _arrivalVelocityMultiplier = 1
+        , _maxRotationVelocity = 0.002
+        , _maxTimesHit = 2
+        }
 
     arrivalMargin :: Double
     arrivalMargin = 200
-
-    arrivalVelocityMultiplier :: Double
-    arrivalVelocityMultiplier = 0.5
 
     data ArrivingDirection = ArrivingDirection
         { _minPositionX :: Double
@@ -119,17 +138,17 @@ module Asteroid
 
     data Orientation = Horizontal | Vertical deriving Eq
 
-    fromLeft :: ArrivingDirection
-    fromLeft = ArrivingDirection
+    fromLeft :: AsteroidInfo -> ArrivingDirection
+    fromLeft AsteroidInfo{_minVelocity, _maxVelocity, _arrivalVelocityMultiplier} = ArrivingDirection
         { _minPositionX = -arrivalMargin
         , _maxPositionX = -arrivalMargin
         , _minPositionY = arrivalMargin
         , _maxPositionY = Constants.nativeHeight - arrivalMargin
 
-        , _minVelocityX = minVelocity
-        , _maxVelocityX = maxVelocity
-        , _minVelocityY = -maxVelocity * arrivalVelocityMultiplier
-        , _maxVelocityY = maxVelocity * arrivalVelocityMultiplier
+        , _minVelocityX = _minVelocity
+        , _maxVelocityX = _maxVelocity
+        , _minVelocityY = -_maxVelocity * _arrivalVelocityMultiplier
+        , _maxVelocityY = _maxVelocity * _arrivalVelocityMultiplier
 
         , _isInBounds = isInBoundsFromLeft
         , _orientation = Horizontal
@@ -138,17 +157,17 @@ module Asteroid
     isInBoundsFromLeft :: Point.Point -> Bool
     isInBoundsFromLeft Point{x} = x > arrivalMargin 
 
-    fromRight :: ArrivingDirection
-    fromRight = ArrivingDirection
+    fromRight :: AsteroidInfo -> ArrivingDirection
+    fromRight AsteroidInfo{_minVelocity, _maxVelocity, _arrivalVelocityMultiplier} = ArrivingDirection
         { _minPositionX = Constants.nativeWidth + arrivalMargin
         , _maxPositionX = Constants.nativeWidth + arrivalMargin
         , _minPositionY = arrivalMargin
         , _maxPositionY = Constants.nativeHeight - arrivalMargin
 
-        , _minVelocityX = -maxVelocity
-        , _maxVelocityX = -minVelocity
-        , _minVelocityY = -maxVelocity * arrivalVelocityMultiplier
-        , _maxVelocityY = maxVelocity * arrivalVelocityMultiplier
+        , _minVelocityX = -_maxVelocity
+        , _maxVelocityX = -_minVelocity
+        , _minVelocityY = -_maxVelocity * _arrivalVelocityMultiplier
+        , _maxVelocityY = _maxVelocity * _arrivalVelocityMultiplier
 
         , _isInBounds = isInBoundsFromRight
         , _orientation = Horizontal
@@ -157,17 +176,17 @@ module Asteroid
     isInBoundsFromRight :: Point.Point -> Bool
     isInBoundsFromRight Point{x} = x < Constants.nativeWidth - arrivalMargin
 
-    fromTop :: ArrivingDirection
-    fromTop = ArrivingDirection
+    fromTop :: AsteroidInfo -> ArrivingDirection
+    fromTop AsteroidInfo{_minVelocity, _maxVelocity, _arrivalVelocityMultiplier} = ArrivingDirection
         { _minPositionX = arrivalMargin
         , _maxPositionX = Constants.nativeWidth - arrivalMargin
         , _minPositionY = -arrivalMargin
         , _maxPositionY = -arrivalMargin
 
-        , _minVelocityX = -maxVelocity * arrivalVelocityMultiplier
-        , _maxVelocityX = maxVelocity * arrivalVelocityMultiplier
-        , _minVelocityY = minVelocity
-        , _maxVelocityY = maxVelocity
+        , _minVelocityX = -_maxVelocity * _arrivalVelocityMultiplier
+        , _maxVelocityX = _maxVelocity * _arrivalVelocityMultiplier
+        , _minVelocityY = _minVelocity
+        , _maxVelocityY = _maxVelocity
 
         , _isInBounds = isInBoundsFromTop
         , _orientation = Vertical
@@ -176,17 +195,17 @@ module Asteroid
     isInBoundsFromTop :: Point.Point -> Bool
     isInBoundsFromTop Point{y} = y > arrivalMargin
 
-    fromBottom :: ArrivingDirection
-    fromBottom = ArrivingDirection 
+    fromBottom :: AsteroidInfo -> ArrivingDirection
+    fromBottom AsteroidInfo{_minVelocity, _maxVelocity, _arrivalVelocityMultiplier} = ArrivingDirection 
         { _minPositionX = arrivalMargin
         , _maxPositionX = Constants.nativeWidth - arrivalMargin
         , _minPositionY = Constants.nativeHeight + arrivalMargin
         , _maxPositionY = Constants.nativeHeight + arrivalMargin
 
-        , _minVelocityX = -maxVelocity * arrivalVelocityMultiplier
-        , _maxVelocityX = maxVelocity * arrivalVelocityMultiplier
-        , _minVelocityY = -maxVelocity
-        , _maxVelocityY = -minVelocity
+        , _minVelocityX = -_maxVelocity * _arrivalVelocityMultiplier
+        , _maxVelocityX = _maxVelocity * _arrivalVelocityMultiplier
+        , _minVelocityY = -_maxVelocity
+        , _maxVelocityY = -_minVelocity
 
         , _isInBounds = isInBoundsFromBottom
         , _orientation = Vertical
@@ -195,17 +214,17 @@ module Asteroid
     isInBoundsFromBottom :: Point.Point -> Bool
     isInBoundsFromBottom Point{y} = y < Constants.nativeHeight - arrivalMargin
 
-    arrivingDirections :: [ArrivingDirection]
-    arrivingDirections = [fromLeft, fromRight, fromTop, fromBottom]
-
-    maxTimesHit :: Integer
-    maxTimesHit = 5
+    arrivingDirections :: AsteroidInfo -> [ArrivingDirection]
+    arrivingDirections asteroidInfo = map (\func -> func asteroidInfo) [fromLeft, fromRight, fromTop, fromBottom]
 
     receiveHit :: Asteroid -> Asteroid
-    receiveHit asteroid@Asteroid{_timesHit} = 
+    receiveHit asteroid@Asteroid{_timesHit, _asteroidType} = 
         let
             timesHit' = _timesHit + 1
-            asteroid' = if timesHit' < 5
+
+            AsteroidType{_asteroidInfo} = _asteroidType
+            AsteroidInfo{_maxTimesHit} = _asteroidInfo
+            asteroid' = if timesHit' < _maxTimesHit
                 then asteroid{_timesHit = timesHit'}
                 else asteroid{_timesHit = timesHit', _isEnabled = False}
         in 
@@ -218,7 +237,7 @@ module Asteroid
             then
                 let
                     asteroid'@Asteroid{_position = position', _velocity = velocity', _rotationVelocity = rotationVelocity', _hasArrived = hasArrived', _arrivingDirection = arrivingDirection'} =
-                        if not _isInitialized then initializeAsteroid asteroid input else asteroid
+                        if not _isInitialized then initializeRandomAsteroid asteroid input else asteroid
                     
                     hasArrived'' = hasArrived' || (_isInBounds arrivingDirection' $ position')
 
@@ -238,24 +257,33 @@ module Asteroid
             else
                 asteroid
 
-    initializeAsteroid :: Asteroid -> Input -> Asteroid
-    initializeAsteroid asteroid Input{randomGenerator} =
+    initializeRandomAsteroid :: Asteroid -> Input -> Asteroid
+    initializeRandomAsteroid asteroid input@Input{randomGenerator} =
         let
-            (arrivingDirectionIndex, randomGenerator1) = Random.randomR (0, length arrivingDirections - 1) randomGenerator
-            arrivingDirection = arrivingDirections !! arrivingDirectionIndex
-            (positionX, randomGenerator2) = Random.randomR (_minPositionX arrivingDirection, _maxPositionX arrivingDirection) randomGenerator1
-            (positionY, randomGenerator3) = Random.randomR (_minPositionY arrivingDirection, _maxPositionY arrivingDirection) randomGenerator2
-            (velocityX, randomGenerator4) = Random.randomR (_minVelocityX arrivingDirection, _maxVelocityX arrivingDirection) randomGenerator3
-            (velocityY, randomGenerator5) = Random.randomR (_minVelocityY arrivingDirection, _maxVelocityY arrivingDirection) randomGenerator4
-            (rotationVelocity, randomGenerator6) = Random.randomR (-maxRotationVelocity, maxRotationVelocity) randomGenerator5
-            (bigAsteroidTypeIndex, _) = Random.randomR (0, length bigAsteroidTypes - 1) randomGenerator6
-            asteroidType = bigAsteroidTypes !! bigAsteroidTypeIndex
+            (bigAsteroidTypeIndex, randomGenerator1) = Random.randomR (0, length bigAsteroidTypes - 1) randomGenerator
+            asteroidType@AsteroidType{_asteroidInfo} = bigAsteroidTypes !! bigAsteroidTypeIndex
+        in
+            initializeAsteroid asteroid asteroidType $ Input.randomizeNext input
+
+    initializeAsteroid :: Asteroid -> AsteroidType -> Input -> Asteroid
+    initializeAsteroid asteroid asteroidType@AsteroidType{_asteroidInfo} Input{randomGenerator} =
+        let
+            arrivingDirections' = arrivingDirections _asteroidInfo
+            (arrivingDirectionIndex, randomGenerator1) = Random.randomR (0, length arrivingDirections' - 1) randomGenerator
+            arrivingDirection' = arrivingDirections' !! arrivingDirectionIndex
+            (positionX, randomGenerator2) = Random.randomR (_minPositionX arrivingDirection', _maxPositionX arrivingDirection') randomGenerator1
+            (positionY, randomGenerator3) = Random.randomR (_minPositionY arrivingDirection', _maxPositionY arrivingDirection') randomGenerator2
+            (velocityX, randomGenerator4) = Random.randomR (_minVelocityX arrivingDirection', _maxVelocityX arrivingDirection') randomGenerator3
+            (velocityY, randomGenerator5) = Random.randomR (_minVelocityY arrivingDirection', _maxVelocityY arrivingDirection') randomGenerator4
+
+            AsteroidInfo{_maxRotationVelocity} = _asteroidInfo
+            (rotationVelocity, _) = Random.randomR (-_maxRotationVelocity, _maxRotationVelocity) randomGenerator5
         in
             asteroid
                 { _position = Point { x = positionX, y = positionY }
                 , _velocity = Point { x = velocityX, y = velocityY }
                 , _rotationVelocity = rotationVelocity
-                , _arrivingDirection = arrivingDirection
+                , _arrivingDirection = arrivingDirection'
                 , _isInitialized = True
                 , _asteroidType = asteroidType
                 }
@@ -266,11 +294,10 @@ module Asteroid
             fragmentTypes = _breaksInto _asteroidType
             fragments = take (length fragmentTypes) $ repeat Asteroid.new
             randomizedInputs = Input.randomize input
-            initializedFragments = zipWith (\fragment randomizedInput -> initializeAsteroid fragment randomizedInput) fragments randomizedInputs
-            initializedFragments' = zipWith (\fragment fragmentType -> fragment{_asteroidType = fragmentType}) initializedFragments fragmentTypes
-            initializedFragments'' = map (\fragment -> fragment{_position = _position, _hasArrived = True}) initializedFragments'
+            initializedFragments = zipWith3 (\fragment fragmentType randomizedInput -> initializeAsteroid fragment fragmentType randomizedInput) fragments fragmentTypes randomizedInputs
+            initializedFragments' = map (\fragment -> fragment{_position = _position, _hasArrived = True}) initializedFragments
         in
-            initializedFragments''
+            initializedFragments'
 
     instance EntityClass Asteroid where
         load asteroid = Sprite.imageDefs asteroid
